5e5143db6236f4f109d2ef305828c05b16a0099a
Lee Massey <lee.m.massey@gmail.com>
2025-11-13 18:06:32 -0800

    Add 5-round functionality

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 1;
 
-function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+    function getComputerChoice() {
+        const choices = ['rock', 'paper', 'scissors'];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
+    }  
+
+    function getHumanChoice() {
+        const choice = prompt('Rock, Paper, or Scissors?');
+        return choice.toLowerCase();
+    }
+
+    function playRound(computerChoice, humanChoice) {
+        let res = 'Computer: ' + computerChoice + ' | Human: ' + humanChoice + ' | ';
+        let winnerChoice;
+        let loserChoice;
+        let winner;
+        if (computerChoice === humanChoice) {
+            return res += 'It\'s a tie! Human Score: ' + humanScore + ' | Computer Score: ' + computerScore;
+        }
+        if (((computerChoice === 'rock') && (humanChoice === 'scissors')) || 
+            ((computerChoice === 'paper') && (humanChoice === 'rock')) || 
+            ((computerChoice === 'scissors') && (humanChoice === 'paper'))) {
+                winnerChoice = computerChoice;
+                loserChoice = humanChoice;
+                winner = 'Computer';
+                computerScore++;
+        }
+        else {
+            loserChoice = computerChoice;
+            winnerChoice = humanChoice;
+            winner = 'Human';
+            humanScore++;
+        }
+        return res += winnerChoice + ' beats ' + loserChoice + '. ' + winner + ' wins this round! Human Score: ' + humanScore + ' | Computer Score: ' + computerScore;
+    }
+
+    while (round <= 5) {
+        console.log(playRound(getComputerChoice(), getHumanChoice()));
+        round++;
+    }
 }
 
-function getHumanChoice() {
-    const choice = prompt('Rock, Paper, or Scissors?');
-    return choice.toLowerCase();
-}
-
-function playRound(computerChoice, humanChoice) {
-    let res = 'Computer: ' + computerChoice + ' | Human: ' + humanChoice + ' | ';
-    let winnerChoice;
-    let loserChoice;
-    let winner;
-    if (computerChoice === humanChoice) {
-        return res += 'It\'s a tie! Human Score: ' + humanScore + ' | Computer Score: ' + computerScore;
-    }
-    if (((computerChoice === 'rock') && (humanChoice === 'scissors')) || 
-        ((computerChoice === 'paper') && (humanChoice === 'rock')) || 
-        ((computerChoice === 'scissors') && (humanChoice === 'paper'))) {
-            winnerChoice = computerChoice;
-            loserChoice = humanChoice;
-            winner = 'Computer';
-            computerScore++;
-    }
-    else {
-        loserChoice = computerChoice;
-        winnerChoice = humanChoice;
-        winner = 'Human';
-        humanScore++;
-    }
-    return res += winnerChoice + ' beats ' + loserChoice + '. ' + winner + ' wins! Human Score: ' + humanScore + ' | Computer Score: ' + computerScore;
-}
-
-const log = playRound(getComputerChoice(), getHumanChoice());
-
-console.log(log);
+playGame();
