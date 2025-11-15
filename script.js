821109e364cb8623463a115a8463598cdcdefce8
Lee Massey <lee.m.massey@gmail.com>
2025-11-15 11:55:07 -0800

    refactor code to fix minor bugs, and build helper functions

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
     let ties = 0;
-    let scoreDiv = document.querySelector('#score');
-    let humanChoiceButtons = document.querySelectorAll('.human-choice-buttons');
-    let resultsDiv = document.querySelector('#results');
-    let displayWinnerDiv = document.querySelector('#display-winner');
+    const scoreDiv = document.querySelector('#score');
+    const humanChoiceButtons = document.querySelectorAll('.human-choice-buttons');
+    const humanChoiceDiv = document.querySelector('#human-choice');
+    const computerChoiceDiv = document.querySelector('#computer-choice');
+    const roundResultDiv = document.querySelector('#round-result');
+    const displayWinnerDiv = document.querySelector('#display-winner');
 
     humanChoiceButtons.forEach((button) => {
         button.addEventListener('click', () => {
-            playRound(getComputerChoice(), button.id);
+            const humanChoice = button.id;
+            const computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
         });
     });
 
@@ -17,36 +21,90 @@ function playGame() {
         const choices = ['rock', 'paper', 'scissors'];
         const randomIndex = Math.floor(Math.random() * choices.length);
         return choices[randomIndex];
-    }  
+    }
 
-    function playRound(computerChoice, humanChoice) {
+    function updateScore() {
+        scoreDiv.textContent = 'Your score: ' + humanScore + ' | Computer score: ' + computerScore + ' | Ties: ' + ties;
+    }
+
+    function updateResultsDiv(humanChoice, computerChoice, roundResult) {
+        if (humanChoice && computerChoice) {
+            humanChoiceDiv.textContent = 'You picked: ' + humanChoice;
+            computerChoiceDiv.textContent = 'Computer picked: ' + computerChoice;
+        }
+        else {
+            humanChoiceDiv.textContent = '';
+            computerChoiceDiv.textContent = '';
+        }
+        roundResultDiv.textContent = roundResult;
+    }
+
+    function reset() {
+        humanScore = 0;
+        computerScore = 0;
+        ties = 0;
+        updateScore();
+        updateResultsDiv(undefined, undefined, 'Make your choice to start the game!');
+        displayWinnerDiv.textContent = '';
+        disableButtons(false);
+    }
+
+    function displayWinner(msg) {
+        displayWinnerDiv.textContent = msg;
+        const resetButton = document.createElement('button');
+        resetButton.textContent = 'Play Again';
+        resetButton.addEventListener('click', reset);
+        displayWinnerDiv.appendChild(resetButton);
+    }
+
+    function disableButtons(flag) {
+        humanChoiceButtons.forEach((button) => {
+            button.disabled = flag;
+        });
+    }
+
+    function checkWinner() {
+        if (humanScore === 5) {
+            displayWinner('You win!!! Game Over!');
+            disableButtons(true);
+        }
+        if (computerScore === 5) {
+            displayWinner('Computer wins!!! Game Over!');
+            disableButtons(true);
+        }
+    }
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    function playRound(humanChoice, computerChoice) {
         let r = 'rock';
         let p = 'paper';
         let s = 'scissors';
-        let res = 'You picked ' + humanChoice + '. Computer picked ' + computerChoice + '. ';
+        let roundResult = '';
         if (humanScore < 5 && computerScore < 5) {
-            if (computerChoice === humanChoice) {
-                res += 'It\'s a tie!';
+            if (humanChoice === computerChoice) {
+                roundResult = 'It\'s a tie!';
                 ties++;
             }
             else if ((computerChoice === r && humanChoice === s) || (computerChoice === p && humanChoice === r) || (computerChoice === s && humanChoice === p)) {
-                res += computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + ' beats ' + humanChoice + '. Computer wins this round!';
+                roundResult = capitalize(computerChoice) + ' beats ' + humanChoice + '. Computer wins this round!';
                 computerScore++;
             }
             else {
-                res += humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ' beats ' + computerChoice + '. Human wins this round!';
+                roundResult = capitalize(humanChoice) + ' beats ' + computerChoice + '. Human wins this round!';
                 humanScore++;
             }
-            resultsDiv.textContent = res;
-            scoreDiv.textContent = 'Your score: ' + humanScore + ' | Computer score: ' + computerScore + ' | Ties: ' + ties;
-            if (humanScore === 5) {
-                displayWinnerDiv.textContent = 'You win!!! Game over!';
-            }
-            if (computerScore === 5) {
-                displayWinnerDiv.textContent = 'Computer wins!!! Game Over!';
-            }
+
+            updateResultsDiv(humanChoice, computerChoice, roundResult);
+            updateScore();
+            checkWinner();
         }
     }
+
+    updateScore();
+    updateResultsDiv(undefined, undefined, 'Make your choice to start the game!');
 }
 
 playGame();
